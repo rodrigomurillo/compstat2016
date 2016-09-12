5036f3ee3db61705ed6813c8fe30f98744b11d5b
Rodrigo <rodrigo.murillo88@gmail.com>
2016-09-12 00:49:12 -0500

    Shiny con la integración de una función usando Montecarlo

---
+++ Montecarlo_Integration/server.R
@@ -0,0 +1,75 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+
+  fun_g <- reactive({
+    texto <- paste("aux <- ", input$funcion)
+    eval(parse(text=texto))
+    aux
+  })
+  
+  output$distPlot <- renderPlot({
+    
+    # generate bins based on input$bins from ui.R
+   
+    n_sim <- input$n_sim
+    n_min = input$n_min
+    n_max = input$n_max
+    alpha = input$alpha
+    #g = input$funcion
+    
+    # 
+    # draw the histogram with the specified number of bins
+    # hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+    # Autor: Rodrigo Murillo
+    # Clave única: 110183
+    # 
+    # Programa para resolver una integral definida usando simulación de uniformes y Montecarlo.
+    #
+    # In: 
+    #     [a, b]: intervalo de integración
+    #     g: función a integrar
+    #     alpha: confianza de los intervalos
+    #
+    #-------------------------------------------
+    a = 0
+    b = 1
+    g = function(x) (log(1/x))^3 - factorial(3)
+    #alpha = 0.05
+    #n_sim = 100
+    #-------------------------------------------
+    library(ggplot2)
+    
+    result = replicate(n_sim, {
+      n = floor(runif(1 ,n_min, n_max))
+      x = runif(n, min=a, max=b)
+      g_x = sapply(x , g)
+      est = mean(g_x)
+      CI = est + c(-1,1)*qnorm(1-alpha/2)*sqrt(var(g_x)/n)
+      (c(n,est,CI[1],CI[2]))
+    })
+    
+    result = as.data.frame(t(result))
+    names(result) = c("Simulaciones","Estimacion","IC_Inferior","IC_Superior")
+    
+    p1 = ggplot(result, aes(x=Simulaciones,y=Estimacion)) + 
+      ggtitle(paste("Estimación Montecarlo con Intervalos al ",(1-alpha)*100,"% de confianza", sep="")) +
+      geom_line(aes(y = Estimacion), colour = "blue") +
+      geom_ribbon(aes(ymin = IC_Inferior, ymax = IC_Superior), alpha = 0.4)
+    
+      p1
+    
+  })
+  
+})
---
+++ Montecarlo_Integration/ui.R
@@ -0,0 +1,44 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Integración Montecarlo $f(x)$"),
+  
+  # Sidebar with a slider input for number of simulations
+  sidebarLayout(
+    sidebarPanel(
+       sliderInput("n_sim",
+                   "Número de puntos a simular:",
+                   min = 10,
+                   max = 1e4,
+                   value = 100)
+    ),
+    
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      textInput(
+        inputId="funcion", 
+        label="Función g",
+        value="function(x) 2*x"
+      ),
+      sliderInput("a", "a", min=-30, max=30, value=0),
+      sliderInput("b", "b", min=-30, max=30, value=1),
+      sliderInput("n_min", "n_min", min=2, max=10000, value=10),
+      sliderInput("n_max", "n_max", min=2, max=10000, value=1000),
+      sliderInput("alpha", "alpha", min=.001, max=.5, value=.05),
+       plotOutput("distPlot")
+    )
+  )
+))
